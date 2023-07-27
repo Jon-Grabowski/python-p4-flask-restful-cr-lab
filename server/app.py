46bc2ff7e5509268844516d6b6094e0aee466b94
Jonathan Grabowski <jon.grabowski@gmail.com>
2023-07-27 19:54:31 -0400

    All tests passing

--- server/app.py
+++ server/app.py
@@ -17,11 +17,32 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        response_dict = [plant.to_dict() for plant in plants]
+        response = make_response(response_dict, 200)
+        return response
+    
+    def post(self):
+        data = request.get_json()
+        plant = Plant(
+            name = data['name'],
+            image = data['image'],
+            price = data['price']
+        )
+        db.session.add(plant)
+        db.session.commit()
+        response = make_response(plant.to_dict(), 200)
+        return response
 
+api.add_resource(Plants, '/plants')
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant = Plant.query.filter(Plant.id == id).first()
+        response = make_response(plant.to_dict(), 200)
+        return response
+
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
